c596c618cb8ac4d3f0c7f6de38ce4a0880fb2e3b
Jack Saunders <61947758+SaundersJE97@users.noreply.github.com>
2022-11-04 12:00:23 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'SaundersJ Blog'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2022, Jack Saunders'
+author = 'Jack Saunders'
 
 release = '0.1'
 version = '0.1.0'
